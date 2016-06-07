4c94d0fe9cbe645e8464955977468236cf4fe058
Freeman Zhang <freeman.zhang1992@gmail.com>
2016-06-07 14:26:23 +0800

    option parsing
    Signed-off-by: Freeman Zhang <freeman.zhang1992@gmail.com>

--- pauser.py
+++ pauser.py
@@ -1,25 +1,81 @@
+#!/usr/bin/python
+
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 import random
 import sys
+import getopt
 
-file_name = sys.argv[1]
+silence_len = 400     # default silence len: 400ms
+silence_thresh = 70  # default silence thresh < -70dBFS
+blank = 1             # default blank len = 1 x chunk len + extra_blank len
+extra_blank = 300
 
-sound = AudioSegment.from_mp3(file_name)
+def usage():
+  print '''\nUsage:
 
-chunks = split_on_silence(sound,min_silence_len=300,silence_thresh=-70)#silence time:700ms and silence_dBFS<-70dBFS
+  python pauser.py -f filename [-s silence_len] [-d silence_thresh] [-b blank]
 
-print "processing ..."
+'''
 
-num_chunks = len(chunks)
-print str(num_chunks) + " pauses injected"
-print "granularity: " + str(len(sound)/num_chunks/1000) + " seconds"
+def evaluate(granularity):
+  if granularity < 3:
+    print "granularity too small!"
+    print "[Suggestion] increase silence length(current is %d ms) via '-s'\n\
+option and/or silence threshold(current is %d) via '-d' option.\n"\
+    %(silence_len, silence_thresh)
+  if granularity > 10:
+    print "granularity too large!"
+    print "[Suggestion] decrease silence length(current is %d ms) via '-s'\n\
+option and/or silence threshold(current is %d) via '-d' option.\n"\
+    %(silence_len, silence_thresh)
 
-new = AudioSegment.empty()
-for i in range(0,num_chunks):
-  new += chunks[i]
-  silence = AudioSegment.silent(duration=len(chunks[i]) + 300)
-  new += silence
+def main(argv):
+  try:
+    opts, args = getopt.getopt(argv[1:], 'hf:s:d:b:', ['help'])
+  except getopt.GetoptError, err:
+    print str(err)
+    usage()
+    sys.exit(2)
+  for opt, val in opts:
+    if opt in ('-h', '--help'):
+      usage()
+      sys.exit(1)
+    elif opt in ('-f'):
+      file_name=val
+    elif opt in ('-s'):
+      silence_len=int(val)
+    elif opt in ('-d'):
+      silence_thresh=int(val);
+    elif opt in ('-b'):
+      blank=int(val);
+    else:
+      print "Invalid option."
+      usage()
+      sys.exit(2)
 
-save_name = "paused-"+file_name
-new.export(save_name, format="mp3")
+  try:
+    sound = AudioSegment.from_mp3(file_name)
+  except:
+    print "\nCannot open file: %s\n"%file_name
+
+  chunks = split_on_silence(sound,min_silence_len=silence_len,silence_thresh=0-silence_thresh)
+  print "processing ..."
+
+  num_chunks = len(chunks)
+  print str(num_chunks) + " pauses injected"
+  granularity = len(sound)/num_chunks/1000;
+  print "granularity: " + str(granularity) + " seconds"
+  evaluate(granularity)
+
+  new = AudioSegment.empty()
+  for i in range(0,num_chunks):
+    new += chunks[i]
+    silence = AudioSegment.silent(duration=blank*len(chunks[i]) + extra_blank)
+    new += silence
+
+  save_name = "paused-"+file_name
+  new.export(save_name, format="mp3")
+
+if __name__ == '__main__':
+    main(sys.argv)
